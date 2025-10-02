7545c83f240d02b9d0f0be2b64bdd6ff930f74f1
Nguyễn Duy Nhất <nguyenduynhat.250505@gmail.com>
2025-10-02 23:14:32 +0700

    Ex10

--- MCU-MPU_Lab1/Core/Src/main.c
+++ MCU-MPU_Lab1/Core/Src/main.c
@@ -42,7 +42,16 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+static GPIO_TypeDef * const LED_PORT[12] = {
+    LED0_GPIO_Port, LED1_GPIO_Port, LED2_GPIO_Port, LED3_GPIO_Port,
+    LED4_GPIO_Port, LED5_GPIO_Port, LED6_GPIO_Port, LED7_GPIO_Port,
+    LED8_GPIO_Port, LED9_GPIO_Port, LED10_GPIO_Port, LED11_GPIO_Port
+};
+static const uint16_t LED_PIN_[12] = {
+    LED0_Pin, LED1_Pin, LED2_Pin, LED3_Pin,
+    LED4_Pin, LED5_Pin, LED6_Pin, LED7_Pin,
+    LED8_Pin, LED9_Pin, LED10_Pin, LED11_Pin
+};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -54,33 +63,16 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-//static void setAllClockOn(void)
-//{
-//    for (int i = 4; i <= 15; i++)  // PA4 = LED1, PA15 = LED12
-//    {
-//        uint16_t pin = (1 << i);
-//        HAL_GPIO_WritePin(GPIOA, pin, GPIO_PIN_RESET); // Active-low: RESET = turn ON, SET = turn OFF
-//    }
-//}
 static void clearAllClock(void)
 {
-    for (int i = 4; i <= 15; i++)  // PA4 = LED1, PA15 = LED12
-    {
-        uint16_t pin = (1 << i);
-        HAL_GPIO_WritePin(GPIOA, pin, GPIO_PIN_SET); // Active-low: RESET = turn ON, SET = turn OFF
+    for (uint8_t i = 0; i < 12; i++) {
+        HAL_GPIO_WritePin(LED_PORT[i], LED_PIN_[i], GPIO_PIN_SET); // OFF
     }
 }
-void setNumberOnClock(int num)
+static void setNumberOnClock(int num)
 {
     if (num < 0 || num > 11) return;
-    uint16_t pin = (uint16_t)(1 << (num + 4)); // PA4 = LED1, PA15 = LED12
-    HAL_GPIO_WritePin(GPIOA, pin, GPIO_PIN_RESET); // Active-low: RESET = turn ON, SET = turn OFF
-}
-void clearNumberOnClock(int num)
-{
-    if (num < 0 || num > 11) return;
-    uint16_t pin = (uint16_t)(1 << (num + 4)); // PA4 = LED1, PA15 = LED12
-    HAL_GPIO_WritePin(GPIOA, pin, GPIO_PIN_SET);     // Active-low: RESET = turn ON, SET = turn OFF
+    HAL_GPIO_WritePin(LED_PORT[num], LED_PIN_[num], GPIO_PIN_RESET);
 }
 void displayClock(int hour, int minute, int second)
 {
@@ -202,16 +194,16 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED1_Pin|LED2_Pin|LED3_Pin|LED4_Pin
-                          |LED5_Pin|LED6_Pin|LED7_Pin|LED8_Pin
-                          |LED9_Pin|LED10_Pin|LED11_Pin|LED12_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, LED0_Pin|LED1_Pin|LED2_Pin|LED3_Pin
+                          |LED4_Pin|LED5_Pin|LED6_Pin|LED7_Pin
+                          |LED8_Pin|LED9_Pin|LED10_Pin|LED11_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED1_Pin LED2_Pin LED3_Pin LED4_Pin
-                           LED5_Pin LED6_Pin LED7_Pin LED8_Pin
-                           LED9_Pin LED10_Pin LED11_Pin LED12_Pin */
-  GPIO_InitStruct.Pin = LED1_Pin|LED2_Pin|LED3_Pin|LED4_Pin
-                          |LED5_Pin|LED6_Pin|LED7_Pin|LED8_Pin
-                          |LED9_Pin|LED10_Pin|LED11_Pin|LED12_Pin;
+  /*Configure GPIO pins : LED0_Pin LED1_Pin LED2_Pin LED3_Pin
+                           LED4_Pin LED5_Pin LED6_Pin LED7_Pin
+                           LED8_Pin LED9_Pin LED10_Pin LED11_Pin */
+  GPIO_InitStruct.Pin = LED0_Pin|LED1_Pin|LED2_Pin|LED3_Pin
+                          |LED4_Pin|LED5_Pin|LED6_Pin|LED7_Pin
+                          |LED8_Pin|LED9_Pin|LED10_Pin|LED11_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
